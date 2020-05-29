9ba43957c38dda1c8d31550ccdb85e3fc57bf43f
lij3 <lij3@epsoft.com.cn>
2020-05-29 14:18:59 +0800

    0529

--- ImoocProject/src/com/imooc/HelloImooc.java
+++ ImoocProject/src/com/imooc/HelloImooc.java
@@ -4,7 +4,7 @@ public class HelloImooc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello,Imooc!");1
+		System.out.println("Hello,Imooc!");
 	}
 
 }
